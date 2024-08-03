b8e73c0d7edc8b4e3714179a93bf43b9a01fe1a3
Hady Awny <hadyawny5@gmail.com>
2024-08-03 18:17:39 +0300

    added keep alive function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { dbConnection } from "./database/dbConnection.js";
 import { bootstrap } from "./src/modules/index.routes.js";
 import dotenv from "dotenv";
 import cors from "cors"
+import { keepAlive } from "./src/utils/keepAlive.js";
 
 
 const app = express();
@@ -12,9 +13,11 @@ app.use(cors())
 
 dotenv.config();
 app.use(express.json());
-// app.use("/uploads",express.static('uploads'));
 
 dbConnection();
 bootstrap(app);
 
 app.listen(process.env.PORT || port, () => console.log(`Job Board Server is listening on port ${process.env.PORT || port}!`));
+
+const keepAliveUrl = `https://jobboardbackend-u9zm.onrender.com`;
+keepAlive(keepAliveUrl);
